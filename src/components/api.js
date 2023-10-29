ac5da5510681ec25433aa5fb53b5e474543609f8
Serhii Haber <haberserhii@icloud.com>
2023-10-29 14:12:40 +0200

    upd

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL =
   'https://652aec3c4791d884f1fd8243.mockapi.io/phonebook-haber';
 
-export const fetchContacts = async () => {
+export const fetchFilms = async () => {
   const response = await axios.get('/contacts');
   return response.data;
 };
